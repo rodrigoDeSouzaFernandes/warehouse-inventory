7b60b4d62c69377168e0d35a0fe616069f6694c8
Rodrigo <rdesouza2008@gmaill.com>
2025-11-14 16:40:38 -0300

    Create add product function

--- Stock.py
+++ Stock.py
@@ -1,4 +1,5 @@
 from models import Product, StockMovement, AddProductDTO
+from datetime import datetime
 
 class Stock:
     def __init__(self):
@@ -11,7 +12,29 @@ class Stock:
     def get_product_by_id(self, id:int) -> Product | None:
         return next((p for p in self.__products if p.id == id), None)
 
-    def add_product(self, movement:StockMovement) -> None:
-        product_exists: Product = self.get_product_by_name(movement.product.name)
+    def add_product(self, product:AddProductDTO) -> None:
+        stock_product: Product = self.get_product_by_name(product.name)
 
+        if stock_product:
+
+            movement = StockMovement()
+            movement.operator = product.operator
+            movement.product = stock_product
+            movement.date = datetime.now()
+
+            stock_product.quantity += product.quantity
+            # call update product function
+
+            # call add stock movement on history function
         
+        else:
+            next_id: int = self.__products[-1].id + 1 if len(self.__products) > 0 else 1
+            new_product: Product = Product()
+            new_product.id = next_id
+            new_product.name = product.name
+            new_product.quantity = product.quantity
+
+            self.__products.append(new_product)
+
+
+
--- models.py
+++ models.py
@@ -11,7 +11,6 @@ class Product:
 class AddProductDTO:
     name: str
     quantity: int
-    date: datetime
     operator: str
 
 @dataclass
