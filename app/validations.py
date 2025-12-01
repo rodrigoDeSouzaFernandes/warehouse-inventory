740d0f69f992a3e1fb8acffff8d4cca5103fae07
Rodrigo <rdesouza2008@gmaill.com>
2025-12-01 12:15:00 -0300

    fix: fix date validation function

--- app/validations.py
+++ app/validations.py
@@ -12,10 +12,8 @@ def is_valid_quanity(value: str) -> bool:
     return isIntegerPositive(value) and value != "0"
 
 def is_valid_date(value: str) -> bool:
-    date_pattern = r"^\d{2}\/\d{2}\/\d{4}$"
-    date_str = re.match(date_pattern, value)
     try:
-        datetime.strptime(date_str, "%d/%m/%Y")
+        datetime.strptime(value, "%d/%m/%Y")
         return True
     except:
         return False
